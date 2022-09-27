e990b36f5b0e383df7d698ea8fcf3a2be10a2d1b
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2022-09-27 13:55:11 -0400

    leet

--- leetcode.py
+++ leetcode.py
@@ -1153,5 +1153,48 @@ Largest Local values in a Matrix
 #                 temp.append(max_loc)
 #             max_arr.append(temp)
 
+'''
+Arrange the listin decreasing order of they=ir frequencies
+'''
+# import collections
+# listEle=[1,2,2,3,3,3,4,4,5,5,5,5,6,6,6,7,8,9,10]
 
+# count_list=collections.Counter(listEle)
+# res=[]
+# for i in count_list:
         
+#         for j in range(count_list[i]):
+#                 res.append(i)
+# print(res)
+
+'''
+Push Dominoes:
+'''
+
+# States for the dominoes:
+        #   • Any triplet that reaches the state 'R.L' remains
+        #     that state permanently.
+        #  
+        #   • These changes occur to pairs that are not part of an 'R.L':
+        #     'R.' --> 'RR', .L' --> 'LL'
+
+        #  Here's the plan:
+        #    1) To avoid the problem with the 'R.L' state when we  address the 
+                                #       'R.' --> 'RR' and  '.L' --> 'LL' changes, we replace each 'R.L' 
+                                #.       with a dummy string (say, 'xxx').
+        #       
+        #    2) We perform the 'R.' --> 'RR', .L' --> 'LL' replacements.
+
+        #    3) Once the actions described in 1) and 2) are completed, we repeat 
+        #       until no changes occur. We replace the dummy string with 'R.L'. 
+# def pushDominoes(self, dominoes: str) -> str:
+#         temp = ''
+
+#         while dominoes != temp:
+#                 temp = dominoes
+#                 dominoes = dominoes.replace('R.L', 'xxx')       # <-- 1)
+#                 dominoes = dominoes.replace('R.', 'RR')         # <-- 2)
+#                 dominoes = dominoes.replace('.L', 'LL')         # <-- 2)
+
+#         return  dominoes.replace('xxx', 'R.L')              # <-- 3)
+
