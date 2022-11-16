9bd5afc852a1c02ccf63290d4985ea8de7f05d89
bhargavi1poyekar <bhargavi1poyekar@gmail.com>
2022-11-16 12:27:22 -0500

    Leet

--- leetcode.py
+++ leetcode.py
@@ -1390,4 +1390,30 @@ Top K Frequent words
         
 #         return [x[0] for x in count_list_sorted[:k]]
         
+'''
+Guess Number Higher or Lower
+'''
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+# class Solution:
+#     def guessNumber(self, n: int) -> int:
+#         low=1
+#         high=n
+#         mid=n//2
+        
+#         while True:
+#             ans=guess(mid)
+#             if ans==-1:
+#                 high=mid
+#             elif ans==1:
+#                 low=mid+1
+#             else:
+#                 return mid
+#             mid=(low+high)//2
+        
         
